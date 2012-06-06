9926ffe2c69537823d1a1b8f89613ef1f62e90df
wush <wush@f2ware.com>
2012-06-06 11:24:26 +0800

    add render

--- src/RmdNode.php
+++ src/RmdNode.php
@@ -9,9 +9,11 @@ class RmdNode
     
     protected $parent;
     
-    protected $attribute = array();
+    protected $_attribute = array();
     
     protected $_child_class = __CLASS__;
+    
+    protected $_content = null;
     
     protected $child = array();
     
@@ -23,12 +25,15 @@ class RmdNode
             switch($key) {
                 case '_attribute':
                     assert(is_array($value));
-                    $this->attribute = $value;
+                    $this->_attribute = $value;
                     break;
                 case '_child_class':
                     assert(is_string($value));
                     $this->_child_class = $value;
                     break;
+                case '_content':
+                    assert(is_string($value));
+                    $this->_content = $value;
                 default:
                     $child_class = $this->_child_class;
                     $this->child[$key] = new $child_class( $key, $level + 1, $value, $this );
@@ -37,8 +42,8 @@ class RmdNode
     }
     
     public function getAttribute($key, $default = null, $inherit = true) {
-        if (array_key_exists($key, $this->attribute)) {
-            return $this->attribute[$key];
+        if (array_key_exists($key, $this->_attribute)) {
+            return $this->_attribute[$key];
         }
         if ($inherit & !is_null($this->parent)) {
             return $this->parent->getAttribute($key, $default, $inherit);
@@ -47,7 +52,20 @@ class RmdNode
     }
     
     public function render() {
-        
+        $retval = $this->getTitle();
+        if (is_null($this->_content)) {
+            $callable = 'renderWithContent';
+        } 
+        else {
+            $callable = 'renderWithoutContent';
+        }
+        foreach ($this->child as $key => $value) {
+            self::$callable($key, $value, $retval);
+        }
+        if (substr($retval, -2, 2) !== str_repeat(self::getBr(), 2)) {
+            $retval .= self::getBr();
+        }
+        return $retval;
     }
     
     private function getTitle() {
@@ -64,5 +82,13 @@ class RmdNode
             default:
                 throw new Exception('TODO: getBr for ' . PHPOS);
         }
+    }
+    
+    static private function renderWithContent($key, RmdNode $value, &$retval) {
+        $retval = str_replace("%$key%", $value->render(), $retval);
+    }
+    
+    static private function renderWithoutContent($key, RmdNode $value, &$retval) {
+        $retval .= $value->render();
     }
 }

d05874271aaa9c5649483c78151dd4bdeff84883
wush <wush@f2ware.com>
2012-06-06 11:35:03 +0800

    update test.sh for more convenient debug settings

--- test/test.sh
+++ test/test.sh
@@ -3,4 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 php -l $1
-php -d "xdebug.auto_trace=1" -d "xdebug.trace_output_dir=$DIR" $1
+rm $DIR/error.log
+php -d "xdebug.auto_trace=1" -d "xdebug.trace_output_dir=$DIR" -d "log_errors=1" -d "error_log=$DIR/error.log" $1
+cat $DIR/error.log
